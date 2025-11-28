431d984318fd154022b2db279c370e15ae3ae0a0
AlessandroHSR <alessandro.hu@galileiostiglia.edu.it>
2025-11-28 10:56:08 +0000

    ciae

--- es1.c
+++ es1.c
@@ -3,6 +3,16 @@
 #include <stdio.h>
 
 int main(){
+    int numero_verysmol, numero_verybigu;
+    printf("inserici un numero");
+    scanf("%d", &numero_verysmol);
+    printf("adesso uno piu grosso");
+    scanf("%d", &numero_verybigu);
 
+    if(numero_verysmol <= numero_verybigu){
+    printf("ok bravo");
     return 0;
+} else {
+    printf("BUGIARDO");
+}
 }
